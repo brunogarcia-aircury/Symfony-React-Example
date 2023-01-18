2c1699b218779ce4f7eac952120b452b400428d9
Bruno García <bruno.garcia@aircury.com>
2023-01-18 17:53:53 +0100

    Test fetch

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,15 +5,27 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     constructor() {
         super();
+
+        this.state = {message: 'loading'};
     }
 
     componentDidMount() {
-
+            fetch('/api/fetch example')
+                 .then(res => res.json())
+                 .then((json)=>{
+                     console.log(json);
+                     return json;
+                 })
+                 .then(json => this.setState({message: json.message}))
+                 .catch(e => console.log(e));
     }
 
     render() {
         return (
-            <>Hello World!!</>
+            <>
+                <h1>Hello World!!</h1>
+                <p>{this.state.message}</p>
+            </>
         );
     }
 }
