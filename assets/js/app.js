392231560ba4a3f3d440264feb684c5f2608749b
Bruno García <bruno.garcia@aircury.com>
2023-01-18 17:35:01 +0100

    React Hello World!! Created a default controller Created a react AppComponent Refactor index.html.twig extends base Refactor base.html.twig to use react

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,27 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// any CSS you import will output into a single css file (app.css in this case)
-import '../styles/app.css';
+console.log('Flag APP');
 
-// start the Stimulus application
-import '../bootstrap';
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            entries: []
+        };
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        console.log('render')
+        return (
+            <>Hello World!!</>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
