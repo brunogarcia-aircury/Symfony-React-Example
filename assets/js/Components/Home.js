5c806ae2d1e1db86f4c3b2bbb889cc0b0c0b5a6f
Bruno García <bruno.garcia@aircury.com>
2023-01-19 18:13:44 +0100

    React router errorElement

--- assets/js/Components/Home.js
+++ assets/js/Components/Home.js
@@ -1,13 +1,24 @@
 import React, {Suspense} from "react";
 import { Link } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
 
 export default class Home extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return <Suspense>
             <ul>
+                <li><Link to='/'>Home</Link></li>
                 <li><Link to='/hello'>Simple Hello</Link></li>
                 <li><Link to='/hello fetch'>Simple Fetch Hello</Link></li>
+                <li><Link to='/_error/404'>Dev 404 error</Link></li>
             </ul>
+            <hr />
+            <div>
+                <Outlet />
+            </div>
         </Suspense>;
     }
 }
