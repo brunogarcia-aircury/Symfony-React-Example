3a4b9a967e8f6f883159c0e04f2826a1638ccc53
Bruno García <bruno.garcia@aircury.com>
2023-01-19 16:40:59 +0100

    Feature basic React router

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 
-class App extends React.Component {
-    constructor() {
-        super();
-    }
 
-    componentDidMount() {
-
-    }
-
-    render() {
-        return (
-            <>
-                <h1>Hello World!!</h1>
-            </>
-        );
-    }
-}
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <h1>Hello world!</h1>,
+    },
+    {
+        path: "/test",
+        element: <h1>This is a test</h1>,
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
