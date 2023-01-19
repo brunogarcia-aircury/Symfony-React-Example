3f00c43c1f3b8daf28c076eefa44d2fcf2629255
Bruno García <bruno.garcia@aircury.com>
2023-01-19 11:43:39 +0100

    Revert Fetch test

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
 class App extends React.Component {
     constructor() {
         super();
-
-        this.state = {message: 'loading'};
     }
 
     componentDidMount() {
-            fetch('/api/fetch example')
-                 .then(res => res.json())
-                 .then((json)=>{
-                     console.log(json);
-                     return json;
-                 })
-                 .then(json => this.setState(c => c.message = json.message))
-                 .catch(e => console.log(e));
+
     }
 
     render() {
         return (
             <>
                 <h1>Hello World!!</h1>
-                <p>{this.state.message}</p>
             </>
         );
     }
